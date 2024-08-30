6aca3faefff03349412569d76e0f84e627807bce
JustinAmstadt <justinamstadt@gmail.com>
2024-08-29 19:16:49 -0700

    fov added

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,11 @@ int main() {
     camera cam;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+    cam.vfov     = 40;
+    cam.lookfrom = point3(0,0,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
+
     cam.render(world, out);
 
     if(outfile.is_open()) {
